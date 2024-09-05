d20b7b3dd6b5fe1099a252a41c8b908ab5468f95
Caroline <deconinck.caro@gmail.com>
2024-09-05 10:24:23 +0200

    first basic app created

---
+++ helloWorld/ageCalculator.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+DateTime birthday = new DateTime(1991,08,30);
+DateTime today = DateTime.Now;
+TimeSpan difference = today.Subtract(birthday);
+
+Console.WriteLine($"You are {Math.Floor(difference.TotalDays/365)} years old.");
