a0306fce7976aaebaa4cf39107c2fbc4e0d5e489
Caroline <deconinck.caro@gmail.com>
2024-09-10 11:15:46 +0200

    working with classes

--- consoleBank/obj/Debug/net8.0/consoleBank.AssemblyInfo.cs
+++ consoleBank/obj/Debug/net8.0/consoleBank.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("consoleBank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20b7b3dd6b5fe1099a252a41c8b908ab5468f95")]
-[assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
-[assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
+[assembly: System.Reflection.AssemblyProductAttribute("consoleBank")]
+[assembly: System.Reflection.AssemblyTitleAttribute("consoleBank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
